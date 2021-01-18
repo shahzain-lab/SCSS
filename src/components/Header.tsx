bf0194be4f4ada6d123eeeb2ca361bd06b52cbf4
shahzain <69709547+shahzain-lab@users.noreply.github.com>
2021-01-18 07:07:59 -0800

    center content

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,13 @@ export const Header = () => {
     return (
         <div className="Header">
             <div className="headTag">
-
+               <h3>Make a website that connects your venue with your fans</h3>
+               <p>We work with you for ypur long term success</p>
+               <input 
+                 type="text"
+                 placeholder="Enter Email"
+               />
+               <button>Submit</button>
             </div>
         </div>
     )
